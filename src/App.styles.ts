565de397416ec07d46f96a886cd8e63210b6847f
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-06-03 14:55:27 -0400

    Iniciado a criação do componente para listar os itens das tarefas

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import styled from "styled-components";
+
 
 export const Container = styled.div`
   background-color: #17181F;
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
 
 const App = () => {
+  const [list, setList] =  useState<Item[]>([
+    {id: 1, name: 'Comprar o pão na padaria', done: false},
+    {id: 2, name: 'Comprar o bolo na padaria', done: false}
+  ]);
   return (
     <C.Container>
       <C.Area>
         <C.Header>Lista de Tarefas</C.Header>
+        
+        {/* Area de adicionar nova tarefa */}
+
+        {list.map((item, index) =>(
+          <ListItem key={index} item={item} />
+        ))}
+
       </C.Area>
     </C.Container>
   );
 }
 
 export default App;
-0h18m
